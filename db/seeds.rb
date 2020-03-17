f9beafbf240b2a670e982e8eb25ab545870eb145
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-17 18:27:21 +0100

    2 users seeded: first with customer and maker, second with only customer

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,12 @@ puts "Destroy all users..."
 User.destroy_all
 puts "Users destroyed"
 
-puts "Create test user..."
+puts "Create test user with customer and maker..."
 test_user = User.create!(email: 'test@example.com', password: "password", password_confirmation: "password")
 test_customer = Customer.create!(user: test_user)
+test_maker = Maker.create!(user: test_user, first_name: "Jean", last_name: "Bon", description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false) )
 puts "  Done. Use 'test@example.com' as login and 'password' as password"
+puts "Create test user with only customer"
+test_user_2 = User.create!(email: 'test2@example.com', password: "password", password_confirmation: "password")
+test_customer_2 = Customer.create!(user: test_user_2)
+puts "  Done. Use 'test2@example.com' as login and 'password' as password"

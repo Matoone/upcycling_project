0e44a7b502116b60569cafb8b7810caa0634acdf
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-17 18:08:50 +0100

    populated seeds.rb with test user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+puts "Destroy all users..."
+User.destroy_all
+puts "Users destroyed"
+
+puts "Create test user..."
+test_user = User.create!(email: 'test@example.com', password: "password", password_confirmation: "password")
+test_customer = Customer.create!(user: test_user)
+puts "  Done. Use 'test@example.com' as login and 'password' as password"

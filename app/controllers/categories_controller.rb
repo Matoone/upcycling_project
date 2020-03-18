428a15dc5c5559fc320bdff57b14e4afa4d383ce
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-18 21:54:58 +0100

    Fixed wrong way to get category items

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,6 @@
 class CategoriesController < ApplicationController
   def show
-    @items = Item.all
-    @category = Category.find_by(id: params[:id])
+    @category = Category.find(params[:id])
     @id = params[:id]
   end
 end

633b773b16fb1e29ff4d2dc2824b2728b1afb71e
whedia <hedia.wejdi94@gmail.com>
2020-03-19 12:21:38 +0100

    update navbar, add root on static_pages and front page index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'items#index'
+  root 'static_pages#index'
 
   resources :shops
   resources :categories

0f876ca06b39f5a02447ab88fcf325f0509be098
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-19 16:39:50 +0100

    Item card add_to_cart button linked to proper controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:update, :show]
   def create
     @user = current_user
     user_cart = Cart.create(customer: @user.customer)
@@ -7,5 +7,22 @@ class CartsController < ApplicationController
 
   def show
     @cart = Cart.find_by(id: params[:id])
+  end
+
+  def update
+    @cart = current_user.customer.cart
+    case params[:operation]
+    when "add"
+      @cart.add_item(params[:item_id])
+      flash[:sucess] = "L'objet' a été ajouté au panier."
+      redirect_to cart_path(@cart.id)
+    when "remove"
+      @cart.remove_item(params[:item_id])
+      flash[:sucess] = "L'objet a été supprimé du panier."
+      redirect_to cart_path(@cart.id)
+    else
+      flash[:error] = "L'opération n'existe pas."
+      redirect_to cart_path(@cart.id)
+    end
   end
 end

6c9865926ed72b96d0f970bc3c6206239e40bf35
Charlie Carpene <solunacisv@gmail.com>
2020-03-19 15:26:31 +0100

    merge commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'items#index'
+  root 'static_pages#index'
 
   resources :shops
   resources :categories
+  resources :items, only: [:show]
 
 end

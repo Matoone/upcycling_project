73537eea71029d2d094ab76cad63ea2036ecf872
Charlie Carpene <solunacisv@gmail.com>
2020-03-19 17:02:43 +0100

    first commit mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   after_create do
     create_customer
+    welcome_send
   end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -14,5 +15,9 @@ class User < ApplicationRecord
   def create_customer
     user_customer = Customer.create!(user: self)
     puts "Customer created id #{user_customer.id}"
+  end
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
   end
 end

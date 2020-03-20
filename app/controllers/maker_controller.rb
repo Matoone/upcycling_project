c1f56ef7ac393a0708bc7ba7e55ce396258c0c1f
whedia <hedia.wejdi94@gmail.com>
2020-03-20 16:20:54 +0100

    Create controller maker

--- app/controllers/maker_controller.rb
+++ app/controllers/maker_controller.rb
@@ -1,2 +1,22 @@
 class MakerController < ApplicationController
+  
+  def create
+    @maker = Maker.new(maker_permitted_params)
+    if current_user.maker.mail
+      flash[:success] = "Votre demande a bien été envoyé"
+      redirect_to edit_user_registration_path
+    else
+      flash[:error] = "Not working"
+      render '/users/edit'
+    end
+  end
+
+  def maker_permitted_params
+    params.permit(:first_name, :last_name)
+  end
+
+  def maker_send
+    UserMailer.become_maker_email(self).deliver_now
+  end
+
 end

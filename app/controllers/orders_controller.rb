18cfdd950d736e1e8530e64aa868758e97a87e42
whedia <hedia.wejdi94@gmail.com>
2020-03-20 10:56:20 +0100

    add api stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,4 +6,25 @@ class OrdersController < ApplicationController
     @items = @cart.cart_sections["#{@section_id}"]
     @shop = Shop.find_by(id: @section_id)
   end
+  
+  def create
+    # Amount in cents
+    @amount = 500
+  
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+  
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'usd',
+    })
+  
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to orders_path
+  end
 end

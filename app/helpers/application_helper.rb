7442625d7608c725059510a3733de86fccc56e8a
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-20 03:14:05 +0100

    Made impossible to add more items to cart than available

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,26 @@ module ApplicationHelper
           "alert alert-info"
     end
   end
+
+  def is_item_quantity_sufficient_to_add_another_to_cart(item_id)
+    if !user_signed_in?
+      return nil
+    end
+    cart_item = current_user.customer.cart.cart_items.find_by(item_id: item_id)
+    if cart_item
+      if cart_item.cart_quantity < cart_item.item.available_quantity
+        return true
+      else
+        return false
+      end
+    else
+      item = Item.find_by(id: item_id)
+      if item.available_quantity > 0
+        return true
+      else
+        return false
+      end
+    end
+  end
+
 end

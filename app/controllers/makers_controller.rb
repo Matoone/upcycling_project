a233063ee8869e5fd4edf21d028dcc8976923f6e
whedia <hedia.wejdi94@gmail.com>
2020-03-20 16:35:39 +0100

    edit controller maker

--- app/controllers/makers_controller.rb
+++ app/controllers/makers_controller.rb
@@ -1,8 +1,10 @@
-class MakerController < ApplicationController
-  
-  def create
+class MakersController < ApplicationController
+  def maker_send
+    UserMailer.become_maker_email(self).deliver_now
+  end
+  def new
     @maker = Maker.new(maker_permitted_params)
-    if current_user.maker.mail
+    if current_user.maker_send
       flash[:success] = "Votre demande a bien été envoyé"
       redirect_to edit_user_registration_path
     else
--- test/controllers/makers_controller_test.rb
+++ test/controllers/makers_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class MakerControllerTest < ActionDispatch::IntegrationTest
+class MakersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end

2a11d705d7a613bfd36a7f0bde8fb0c0c2f7959d
Charlie Carpene <solunacisv@gmail.com>
2020-03-20 13:36:06 +0100

    adding address form to user.customer profil

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,21 +1,37 @@
 class AddressesController < ApplicationController
   def create
-    puts "-" * 30
-    @permitted_params = params.permit(:first_name, :last_name, :street_number, :address_line_1, :address_line_2, :zip_code, :city)
-    @address = Address.new(@permitted_params)
-    puts @address.street_number
+    @address = Address.new(address_permitted_params)
     current_user.customer.address = @address
-    current_user.customer.save
-    p current_user.customer.address.errors.messages
-    #current_user.customer.address = @address
-    #current_user.customer.address = @address
-    puts "-" * 30
+    if current_user.customer.save
+      puts "-" * 30
+      puts "yeah"
+      p @address
+      puts "-" * 30
+      redirect_to '/users/edit'
+    else
+      puts "-" * 30
+      puts "nop"
+      puts "-" * 30
+      render '/users/edit'
+    end
   end
 
   def update
-    puts "-" * 30
-    puts "yeah"
-    puts params
-    puts "-" * 30
+    @address = current_user.customer.address
+    @address = address_permitted_params
+    if current_user.customer.save
+      puts "-" * 30
+      puts "yeah"
+      p @address
+      puts "-" * 30
+    else
+      puts "-" * 30
+      puts "nop"
+      puts "-" * 30
+    end
+  end
+
+  def address_permitted_params
+    params.permit(:first_name, :last_name, :street_number, :address_line_1, :address_line_2, :zip_code, :city)
   end
 end

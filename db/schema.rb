e905e545fa73da646de3c6cecf47538826c81907
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-23 11:58:34 +0100

    schema after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_161048) do
+ActiveRecord::Schema.define(version: 2020_03_23_103932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_03_20_161048) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_customer_id"
     t.index ["user_id"], name: "index_customers_on_user_id"
   end
 

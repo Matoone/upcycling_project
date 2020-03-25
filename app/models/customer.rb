254fcff8415abb46f53c725716294efe6ad57dec
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-25 15:21:32 +0100

    create method for customer identification check

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,22 +7,21 @@ class Customer < ApplicationRecord
   has_one :cart, dependent: :destroy
   # has_many :orders
 
-  private
-  
-  def create_customer_cart
-    cart = Cart.create(customer: self)
-    puts "Cart created id #{cart.id}"
-  end
-
   def is_own_identity(customer_id)
-    customer = Customer.find_by(id: customer_id)
-    if !customer
-      return false
-    elsif customer.id != self.id
+    if customer_id != self.id
       return false
     else
       return true
     end
   end
+
+  private
+
+  def create_customer_cart
+    cart = Cart.create(customer: self)
+    puts "Cart created id #{cart.id}"
+  end
+
+ 
 
 end

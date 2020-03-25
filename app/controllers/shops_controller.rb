036c7cd1fc8a45984483f790080f3000b23ed98d
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-25 15:24:45 +0100

    create before action identification  check for shop

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,15 @@
 class ShopsController < ApplicationController
+  include ShopsHelper
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action only: [:edit, :update, :destroy] do
+    maker = Shop.find_by(id: params[:id]).maker
+    if !current_user.maker || !current_user.maker.is_own_identity(maker.id)
+      flash[:alert] = "Vous n'êtes pas autorisé à accéder à la page d'administration de ce shop"
+      redirect_to root_path
+    end
+  end
+  # will have to check for new and create when implemented
+
   def show
     @shop = Shop.find_by(id: params[:id])
    

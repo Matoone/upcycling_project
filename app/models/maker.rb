269f087a2540298b4013ca8f627683feed4b4119
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-25 15:22:16 +0100

    create method for maker identification check

--- app/models/maker.rb
+++ app/models/maker.rb
@@ -5,5 +5,12 @@ class Maker < ApplicationRecord
   has_one :address, dependent: :destroy
   has_one :shop, dependent: :destroy
 
-
+  def is_own_identity(maker_id)
+    if maker_id != self.id
+      return false
+    else
+      return true
+    end
+  end
+  
 end

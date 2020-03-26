bcd96411f10c939f915d485d8114553bf4b2d0c4
Charlie Carpene <solunacisv@gmail.com>
2020-03-26 18:11:09 +0100

    set serve_static_assets and assets.compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,17 +26,10 @@ Rails.application.configure do
   config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  # Generate digests for assets URLs
-  config.assets.digest = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.

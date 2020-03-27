34d2c8601f46488ed0988c4af989c290c5f6b808
Matthieu Degré <matthieu.degre@gmail.com>
2020-03-27 19:27:32 +0100

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,15 +37,19 @@ home_category = Category.create!(name: "Maison")
 lifestyle_category = Category.create!(name: "Vie Quotidienne")
 child_category = Category.create!(name: "Enfant")
 
+realNames = [{:first_name => "Marc", :last_name => "Dutrot"}, {:first_name => "Jean", :last_name => "Maljean"}, {:first_name => "Sigmund", :last_name => "Friend"}]
+realAddresses = [{:address_line_1 => "14 Rue Jarente", :zip_code => "69002", :city => "Lyon"}, {:address_line_1 => "31 rue du Boeuf", :zip_code => "69005", :city => "Lyon"}, {:address_line_1 => "19 Rue Verlet Hanus", :zip_code => "69003", :city => "Lyon"}]
+realDescriptions = ["Travailleur du cuir et maroquinier depuis 20 ans, j'ai décidé d'adhérer à la démarche de l'upcycling. A très vite sur mon shop !", "Je suis un fabricant de chaussures expérimenté et habile qui prend son métier à coeur.", "Ancien banquier, j'ai tout plaqué pour me reconvertir dans la fabrication d'objets revalorisés."]
+realShopNames = ["Dutrot & Co", "Gémal Opié Shop", "Trendy Shop"]
 
 
 puts "Create test users with customer and maker, shop and items..."
-5.times do |i|
+3.times do |i|
   user = User.create!(email: Faker::Internet.email, password: "password", password_confirmation: "password")
-  user_maker = Maker.create!(user: user, description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false) )
-  shop =  Shop.create!(maker: user.maker, name: Faker::Commerce.department(max: 3))
-  customer_address = Address.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name , address_line_1: "rue " + Faker::Address.street_name, zip_code: "69100", city: Faker::Address.city, customer: user.customer)
-  maker_address = Address.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name , address_line_1: "rue " + Faker::Address.street_name, zip_code: "69100", city: Faker::Address.city, maker: user.maker)
+  user_maker = Maker.create!(user: user, description: realDescriptions[i] )
+  shop =  Shop.create!(maker: user.maker, name: realShopNames[i])
+  customer_address = Address.create!(first_name: realNames[i][:first_name], last_name: realNames[i][:last_name], address_line_1: realAddresses[i][:address_line_1], zip_code: realAddresses[i][:zip_code], city: realAddresses[i][:city], customer: user.customer)
+  maker_address = Address.create!(first_name: realNames[i][:first_name], last_name: realNames[i][:last_name], address_line_1: realAddresses[i][:address_line_1], zip_code: realAddresses[i][:zip_code], city: realAddresses[i][:city], maker: user.maker)
 
   10.times do
     item = Item.create!(name: Faker::Commerce.product_name, description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), price: Faker::Commerce.price(range: 1..70.0), available_quantity: Faker::Number.within(range: 1..10), shop: shop, category: Category.all.sample)
@@ -53,49 +57,12 @@ puts "Create test users with customer and maker, shop and items..."
 end
 
 
-
-puts "Create test users with customer only..."
-5.times do |i|
-  user = User.create!(email: Faker::Internet.email, password: "password", password_confirmation: "password")
-  customer_address = Address.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name , address_line_1: "rue " + Faker::Address.street_name, zip_code: "69100", city: Faker::Address.city, customer: user.customer)
-  user.save
-end
-
-
-# ecological_toothbrush = Item.create!(name: "Brosse à dents écologique", description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), price: 4.2, available_quantity: 5, shop: user_1_maker_shop, category: bathroom_category)
-# biologic_tisane = Item.create!(name: "Tisane biologique des elfes", description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), price: 7, available_quantity: 15, shop: user_1_maker_shop, category: grocery_category)
-# handmade_statue = Item.create!(name: "Statue décorative faite à la main", description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), price: 35, available_quantity: 1, shop: user_1_maker_shop, category: decoration_category)
-# bulk_bags = Item.create!(name: "Sacs à vrac", description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), price: 30, available_quantity: 3, shop: user_1_maker_shop, category: grocery_category)
-# coat_rack = Item.create!(name: "Porte manteau", description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), price: 50, available_quantity: 1, shop: user_1_maker_shop, category: decoration_category)
-# earrings = Item.create!(name: "Boucle d'oreille", description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), price: 7, available_quantity: 10, shop: user_1_maker_shop, category: grocery_category)
-# make_up_remover = Item.create!(name: "Coton démaquillant réutilisable", description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), price: 2, available_quantity: 25, shop: user_1_maker_shop, category: bathroom_category)
-# wooden_stick = Item.create!(name: "Baguettes", description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), price: 6, available_quantity: 4, shop: user_1_maker_shop, category: grocery_category)
-
-
-puts "Push some items into carts"
-Customer.all.each do |customer|
-  5.times do
-    customer.cart.items.push(Item.all.sample)
-  end
-end
-
-
 puts "Create test user: you can connect with him with email: test@yopmail.com and password: azerty"
 user = User.create!(email: "test@yopmail.com", password: "azerty", password_confirmation: "azerty", is_admin: true)
-user_maker = Maker.create!(user: user, description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false) )
-shop =  Shop.create!(maker: user.maker, name: Faker::Commerce.department(max: 3))
-customer_address = Address.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name , address_line_1: "rue " + Faker::Address.street_name, zip_code: "69100", city: Faker::Address.city, customer: user.customer)
-maker_address = Address.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name , address_line_1: "rue " + Faker::Address.street_name, zip_code: "69100", city: Faker::Address.city, maker: user.maker)
-10.times do
-  item = Item.create!(name: Faker::Commerce.product_name, description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), price: Faker::Commerce.price(range: 0..70.0), available_quantity: Faker::Number.within(range: 1..10), shop: shop, category: Category.all.sample)
-end
+
 
 Maker.all.each do |maker|
   maker.is_validated = true
   maker.save
 end
 
-# puts "Create fake order"
-
-#   Order.create!(stripe_customer_id: Faker::Code.imei, customer_id: Customer.all.last.id, shop_id: user_1.maker.shop.id)
-#   puts "Done"
